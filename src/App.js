4688aaa4c17375ad241dd4b7ad0232658f30e734
Jerkbert <raballantyne@gmail.com>
2020-01-23 12:14:21 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="main-page">
-        <h1>WELCOME TO TRIVIA STUD!</h1>
+        <h1 className="main-title">ScoreKeeper</h1>
 
         <NewTeamForm addTeam={this.addTeam} />
 
